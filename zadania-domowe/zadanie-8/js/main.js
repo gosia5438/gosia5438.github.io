99c184e0912018612a0c4b28473758f11179d1d0
Malgorzata Maciejowska <malgorzatamaciejowska21@gmail.com>
2018-02-02 22:11:35 +0100

    zadanie ajax

--- zadania-domowe/zadanie-8/js/main.js
+++ zadania-domowe/zadanie-8/js/main.js
@@ -1,14 +1,17 @@
-'use strict';
-
-$(function () {    
-    $('button').click(function(){    
-        $.getJSON("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php", function(data) {
-        console.log(data);
-            
-        var paragraf = $('p');
-        var paragrafText = $(document.createTextNode('User ID jest rowne: ' + data.imie + ' UserName jest rowne ' + data.nazwisko + ' User URL jest rowne ' + data.zawod));
-        $('p').append(paragrafText);
-        $('body').append(paragraf);
+ 'use strict';
+    $(function () {
+        $("#button").on({
+            "click": function pobierzDane() {
+                $.getJSON("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php", function (data) {
+                    var container = $('<div/>', {
+                        id: 'dane-programisty',
+                                          });
+                    $(container).html("Imię: " + data.imie + "<br>" + "Nazwisko: " + data.nazwisko + "<br>" + "Zawód: " + data.zawod + "<br>" + "Firma: " + data.firma);
+                    $("body").append(container);
+                })
+            }
+        })
+        $("#button").onclick(function () {
+            "console.log(’test’)"
         });
     });
-});
